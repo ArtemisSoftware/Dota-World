7009cc81d6f6a713d63f35eb9a1c8a62ac0aa76b
gustavo <gustavo.isaac@gmail.com>
2021-09-22 09:13:46 +0100

    Compose Navigation Transition Animations API

--- app/src/main/java/com/artemissoftware/dotaworld/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/dotaworld/MainActivity.kt
@@ -3,14 +3,14 @@ package com.artemissoftware.dotaworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import coil.ImageLoader
 import com.artemissoftware.dotaworld.ui.navigation.Screen
 import com.artemissoftware.dotaworld.ui.theme.DotaWorldTheme
@@ -18,14 +18,14 @@ import com.artemissoftware.ui_herodetail.HeroDetail
 import com.artemissoftware.ui_herodetail.ui.HeroDetailViewModel
 import com.artemissoftware.ui_herolist.HeroList
 import com.artemissoftware.ui_herolist.ui.HeroListViewModel
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-
-
 
     @Inject
     lateinit var imageLoader: ImageLoader
@@ -40,19 +40,25 @@ class MainActivity : ComponentActivity() {
             DotaWorldTheme {
 
 
-                val navController = rememberNavController()
+                val navController = rememberAnimatedNavController()
 
-                NavHost(
-                    navController = navController,
-                    startDestination = Screen.HeroList.route,
-                    builder = {
-                        addHeroList(
-                            navController = navController,
-                            imageLoader = imageLoader
-                        )
-                        addHeroDetail(imageLoader = imageLoader)
-                    }
-                )
+                BoxWithConstraints {
+                    AnimatedNavHost(
+                        navController = navController,
+                        startDestination = Screen.HeroList.route,
+                        builder = {
+                            addHeroList(
+                                navController = navController,
+                                imageLoader = imageLoader,
+                                width = constraints.maxWidth / 2,
+                            )
+                            addHeroDetail(
+                                imageLoader = imageLoader,
+                                width = constraints.maxWidth / 2,
+                            )
+                        }
+                    )
+                }
 
             }
         }
@@ -68,9 +74,28 @@ class MainActivity : ComponentActivity() {
 fun NavGraphBuilder.addHeroList(
     navController: NavController,
     imageLoader: ImageLoader,
+    width: Int
 ) {
     composable(
         route = Screen.HeroList.route,
+        exitTransition = {_, _ ->
+            slideOutHorizontally(
+                targetOffsetX = { -width },
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = FastOutSlowInEasing
+                )
+            ) + fadeOut(animationSpec = tween(300))
+        },
+        popEnterTransition = { initial, _ ->
+            slideInHorizontally(
+                initialOffsetX = { -width },
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = FastOutSlowInEasing
+                )
+            ) + fadeIn(animationSpec = tween(300))
+        },
     ){
         val viewModel: HeroListViewModel = hiltViewModel()
 
@@ -85,10 +110,30 @@ fun NavGraphBuilder.addHeroList(
     }
 }
 
-fun NavGraphBuilder.addHeroDetail(imageLoader: ImageLoader) {
+@ExperimentalAnimationApi
+fun NavGraphBuilder.addHeroDetail(imageLoader: ImageLoader, width: Int) {
     composable(
         route = Screen.HeroDetail.route + "/{heroId}",
         arguments = Screen.HeroDetail.arguments,
+
+        enterTransition = { _, _ ->
+            slideInHorizontally(
+                initialOffsetX = { width },
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = FastOutSlowInEasing
+                )
+            ) + fadeIn(animationSpec = tween(300))
+        },
+        popExitTransition = { _, target ->
+            slideOutHorizontally(
+                targetOffsetX = { width },
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = FastOutSlowInEasing
+                )
+            ) + fadeOut(animationSpec = tween(300))
+        },
     ){
 
         val viewModel: HeroDetailViewModel = hiltViewModel()
