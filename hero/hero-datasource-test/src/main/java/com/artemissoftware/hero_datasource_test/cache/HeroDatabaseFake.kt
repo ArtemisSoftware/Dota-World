26bac91808c283a8c883ca9f9e7a74667d02b9e5
gustavo <gustavo.isaac@gmail.com>
2021-09-24 18:38:01 +0100

    Fake Caching Datasource

---
+++ hero/hero-datasource-test/src/main/java/com/artemissoftware/hero_datasource_test/cache/HeroDatabaseFake.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.hero_datasource_test.cache
+
+import com.artemissoftware.hero_domain.Hero
+
+class HeroDatabaseFake {
+
+    val heros: MutableList<Hero> = mutableListOf()
+}
